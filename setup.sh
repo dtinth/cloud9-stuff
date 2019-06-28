15653646312b8c4a4c11da9f4dd9ec64d6c818a8
Thai on Cloud9 <dtinth@spacet.me>
2019-06-28 07:56:40 +0000

    my, fish

--- setup.sh
+++ setup.sh
@@ -92,6 +92,53 @@ else
 fi
 
 echo
+echo '# Setup ~/environment/my'
+if ! [ -e ~/environment/my ]
+then
+  git clone git@github.com:dtinth/my.git ~/environment/my
+  echo "  * ~/environment/my is set up."
+else
+  echo "  * Already done."
+fi
+
+echo
+echo '# Setup ~/my'
+if ! [ -e ~/my ]
+then
+  ln -s ~/environment/my ~/my
+  echo "  * ~/my is set up."
+else
+  echo "  * Already done."
+fi
+
+echo
+echo '# Setup fish'
+if ! [ -x "$(command -v fish)" ]
+then
+  sudo wget -O /etc/yum.repos.d/shells:fish:release:3.repo https://download.opensuse.org/repositories/shells:fish:release:3/RedHat_RHEL-6/shells:fish:release:3.repo
+  sudo yum install -y fish
+  sudo chsh -s /usr/bin/fish ec2-user
+  echo "  * fish is set up."
+else
+  echo "  * Already done."
+fi
+
+echo
+echo '# Setup oh-my-fish'
+if [ -x "$(command -v fish)" ]
+then
+  if ! [ -e ~/.local/share/omf ]
+  then
+    curl -L https://get.oh-my.fish > /tmp/omf-installer
+    fish /tmp/omf-installer --path=~/.local/share/omf --config=~/my/omf
+  else
+    echo "  * Already done."
+  fi
+else
+  echo "  * fish is not available."
+fi
+
+echo
 echo '# Setup bashrc'
 if ! grep -q 'cloud9-stuff/\.bashrc' ~/.bashrc
 then
